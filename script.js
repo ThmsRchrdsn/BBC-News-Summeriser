9005f2163a2c0dc3801900d44692f39f1a93b2f7
ThmsRchrdsn <115042486+ThmsRchrdsn@users.nereply.github.com>
2023-02-17 21:57:37 +0000

    adding articles

---
+++ script.js
@@ -0,0 +1,50 @@
+let al = document.querySelectorAll("#article-link");
+let acs = document.querySelectorAll("#article-content-summary");
+let ah = document.querySelectorAll("#article-heading");
+
+// const url = "https://www.bbc.co.uk/news";
+
+window.onload = fireEvent();
+
+let links = [];
+let contents = [];
+let summaries = [];
+
+async function fireEvent() {
+    const response = await fetch('https://www.bbc.com/news');
+    const data = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, 'text/html');
+    for (let i = 0; i < 10; i++) {
+        const most_popular = doc.querySelectorAll('li[data-entityid="most-popular-read-'+(i+1)+'"]');
+        const firstHeadline = most_popular[0];
+        const link = firstHeadline.querySelector('a').getAttribute('href');
+        links.push('https://www.bbc.com' + link);
+        al[i].innerText = 'https://www.bbc.com' + link;
+        ah[i].innerText = i;
+    }
+    for (let i = 0; i < 10; i++) {
+        console.log(links[i]);
+        const text = await get_bbc_text(links[i]);
+        contents.push(text);
+        acs[i].innerText = text;
+    }
+}
+
+async function get_bbc_text(url) {
+    const response = await fetch(url);
+    const data = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, 'text/html');
+    const body = doc.body;
+    const root = body.querySelector('main');
+    let text = "";
+    if (root != null){
+        const comps = root.querySelectorAll('div[data-component="text-block"]');
+        for (const block of comps) {
+          text += block.textContent;
+        }
+    }
+    return text;
+}
+  
